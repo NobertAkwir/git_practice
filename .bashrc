f242a5b46b296ec0137a322e4d6b623aba38d35f
NobertAkwir <nobertakwirogwel@gmail.com>
2022-09-13 15:49:10 +0300

    first codes

---
+++ .bashrc
@@ -0,0 +1,2 @@
+export PATH="$PATH:/c/Users/DELL/anaconda3:/c/Users/DELL/anaconda3/Scripts"
+alias python ="winpty python.exe"
---
+++ data_analysis_intro.py
@@ -0,0 +1,9 @@
+info_table = [
+    {"color": "green", "number": 7},
+    {"color": "red", "number": 2},
+    {"color": "orange", "number": 1}
+]
+
+info_table[1]
+
+type(info_table) 
